bd77a381032debccfcfc13a3ac76f4635474c4da
Kaustav Ghosh <teetangh@gmail.com>
2022-03-11 19:20:27 +0530

    made non-sticky and non-overlapping footer

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,102 @@
 import React from 'react';
+import "./HomePage.css";
 
 const HomePage = () => {
   return (
-    <div>
-        Homepage
+    <div className="home_page">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nostrum, voluptates et vero non quasi? Nisi distinctio eaque at quidem beatae, modi iure ipsum, adipisci velit alias sint, tempore cupiditate?
+      </p>
+
+
     </div>
   );
 }

1552a3be3a5e70a0de47e7266d6eaffc4b580e76
Kaustav Ghosh <teetangh@gmail.com>
2022-03-14 00:01:32 +0530

    updated folder heirarchy + moved files + restructured imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
-import AboutPage from './pages/AboutPage';
-import HomePage from './pages/HomePage';
-// import LoginPage from './pages/LoginPage';
-// import SignupPage from './pages/SignupPage';
-import NotFoundPage from './pages/NotFoundPage';
-// import ContactPage from './pages/ContactPage';
-import PricingPage from './pages/PricingPage';
-import ProductCategoriesPage from './pages/ProductCategoriesPage';
-import ProductSinglePage from './pages/ProductSinglePage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import HomePage from './pages/HomePage/HomePage';
+// import LoginPage from './pages/LoginPage/LoginPage';
+// import SignupPage from './pages/SignupPage/SignupPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+// import ContactPage from './pages/ContactPage/ContactPage';
+import PricingPage from './pages/PricingPage/PricingPage';
+import ProductCategoriesPage from './pages/ProductCategoriesPage/ProductCategoriesPage';
+import ProductSinglePage from './pages/ProductSinglePage/ProductSinglePage';
 
 
 function App() {

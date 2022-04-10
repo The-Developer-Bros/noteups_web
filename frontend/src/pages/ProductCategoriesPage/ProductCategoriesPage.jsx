5941c47abcb0f223b0bdb0812b1fb3a4fe2d9dee
Kaustav Ghosh <teetangh@gmail.com>
2022-04-10 15:51:49 +0530

    added dynamic search functionality with search button

--- frontend/src/pages/ProductCategoriesPage/ProductCategoriesPage.jsx
+++ frontend/src/pages/ProductCategoriesPage/ProductCategoriesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductListing from './ProductListing';
 
 import { useDispatch } from 'react-redux';
@@ -7,19 +7,42 @@ import { fetchAsyncEngineeringSubjects, fetchAsyncArtsSubjects, fetchAsyncCommer
 const ProductCategoriesPage = () => {
 
   const dispatch = useDispatch();
-  const searchText = "computer";
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
     dispatch(fetchAsyncEngineeringSubjects(searchText));
     dispatch(fetchAsyncArtsSubjects(searchText));
     dispatch(fetchAsyncCommerceSubjects(searchText));
-  }, [dispatch]);
+  }, [searchText, dispatch]);
 
-
+  // Backup when user clicks on the search button
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (searchText === "" || searchText === " ") {
+      alert("Please enter a search searchText");
+      return;
+    } else {
+      dispatch(fetchAsyncEngineeringSubjects(searchText));
+      dispatch(fetchAsyncArtsSubjects(searchText));
+      dispatch(fetchAsyncCommerceSubjects(searchText));
+    }
+    setSearchText("");
+  }
 
   return (
     <div>
       <div className="banner-img">
+      </div>
+      <div className="search-bar">
+        <form onSubmit={submitHandler}>
+          <input
+            type="text"
+            placeholder="Search Movies"
+            onChange={(e) => setSearchText(e.target.value)} />
+          <button type="submit">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
       </div>
       <ProductListing />
     </div>

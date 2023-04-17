d098fe62f0df451d5d8252061156d0b074cd5c15
teetangh <teetangh@gmail.com>
2023-04-17 22:35:18 +0530

    updated passport social oauth and npm backend packages

--- backend/services/PassportOAuthService.js
+++ backend/services/PassportOAuthService.js
@@ -1,15 +1,16 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
+const GitHubStrategy = require("passport-github2").Strategy;
 
 const mongoose = require("mongoose");
 // const User = mongoose.model("users");
 const { User } = require("../models/UserModel");
 
+const stripeService = require("../services/payments/StripeService");
+
 const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
-
-const stripeService = require("../services/payments/StripeService");
 
 // passport.serializeUser((user, done) => {
 //   done(null, user.id);
@@ -44,105 +45,6 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL: "/auth/google/callback",
-//       passReqToCallback: true,
-//     },
-//     (req, accessToken, refreshToken, profile, done) => {
-//       try {
-//         if (req.user) {
-//           User.findOne({ googleId: profile.id }, (err, existingUser) => {
-//             if (err) {
-//               console.log("error finding user", err);
-//               return done(err);
-//             }
-//             if (existingUser) {
-//               console.log("existing user", existingUser);
-//               done(null, existingUser);
-//             } else {
-//               console.log("new user", profile);
-//               const user = new User();
-//               user.googleId = profile.id;
-//               user.tokens.push({ kind: "google", accessToken });
-//               user.profile.name = profile._json.name;
-//               user.profile.gender = profile._json.gender;
-//               user.profile.picture = profile._json.picture;
-
-//               user.name = profile.displayName;
-//               user.email = profile.emails[0].value;
-
-//               user.save((err) => {
-//                 if (err) {
-//                   console.log("error saving user", err);
-//                   done(err);
-//                 } else {
-//                   console.log("saved user", user);
-//                   done(null, user);
-//                 }
-//               });
-//             }
-//           });
-//         } else {
-//           User.findOne({ googleId: profile.id }, (err, existingUser) => {
-//             if (err) {
-//               console.log("error finding user", err);
-//               done(err);
-//             } else if (existingUser) {
-//               console.log("existing user", existingUser);
-//               done(null, existingUser);
-//             } else {
-//               User.findOne(
-//                 { email: profile.emails[0].value },
-//                 (err, existingEmailUser) => {
-//                   if (err) {
-//                     console.log("error finding user", err);
-//                     done(err);
-//                   } else if (existingEmailUser) {
-//                     console.log("existing email user", existingEmailUser);
-//                     done(null, existingEmailUser);
-//                   } else {
-//                     console.log("new user", profile);
-//                     const user = new User();
-
-//                     user.googleId = profile.id;
-//                     user.tokens.push({ kind: "google", accessToken });
-//                     user.profile.name = profile._json.name;
-//                     user.profile.gender = profile._json.gender;
-//                     user.profile.picture = profile._json.picture;
-
-//                     user.name = profile.displayName;
-//                     user.email = profile.emails[0].value;
-
-//                     user.save((err) => {
-//                       if (err) {
-//                         console.log("error saving user", err);
-//                         done(err);
-//                       } else {
-//                         console.log(
-//                           "Google account has been linked to your account",
-//                           user
-//                         );
-//                         done(null, user);
-//                       }
-//                     });
-//                   }
-//                 }
-//               );
-//             }
-//           });
-//         }
-//       } catch (err) {
-//         console.log("error", err);
-//         done(err);
-//       }
-//     }
-//   )
-// );
-
 passport.use(
   new GoogleStrategy(
     {
@@ -161,99 +63,98 @@ passport.use(
 
       try {
         if (req.user) {
-          User.findOne({ googleId: profile.id }, (err, existingUser) => {
-            if (err) {
-              return done(err);
+          const existingUser = await User.findOne({ googleId: profile.id });
+
+          if (existingUser) {
+            console.log("errors", {
+              msg: "There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.",
+            });
+            done(null, existingUser);
+          } else {
+            const user = await User.findById(req.user.id);
+
+            if (!user) {
+              return done(null, false);
             }
-            if (existingUser) {
-              console.log("errors", {
-                msg: "There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.",
-              });
-              // done(err);
-              done(null, existingUser);
-            } else {
-              User.findById(req.user.id, async (err, user) => {
-                if (err) {
-                  return done(err);
-                }
 
-                user.googleId = profile.id;
-                user.tokens.push({ kind: "google", accessToken });
-                user.profile.name = profile._json.name;
-                user.profile.gender = profile._json.gender;
-                user.profile.picture = profile._json.picture;
+            user.googleId = profile.id;
+            user.tokens.push({ kind: "google", accessToken });
+            user.profile.name = profile._json.name;
+            user.profile.gender = profile._json.gender;
+            user.profile.picture = profile._json.picture;
 
-                user.name = profile.displayName;
-                user.email = profile.emails[0].value;
+            user.name = profile.displayName;
+            user.email = profile.emails[0].value;
 
-                // Create Stripe Customer
-                const customer = await stripeService.addNewCustomer(
-                  user.profile.name,
-                  user.email
-                );
-                user.stripeCustomerId = customer.id;
-
-                user.save((err) => {
-                  console.log("info", {
-                    msg: "Google account has been linked.",
-                    user: user,
-                    customer: customer,
-                  });
-                  done(err, user);
-                });
-              });
-            }
-          });
-        } else {
-          User.findOne({ googleId: profile.id }, (err, existingUser) => {
-            if (err) {
-              return done(err);
-            }
-            if (existingUser) {
-              return done(null, existingUser);
-            }
-            User.findOne(
-              { email: profile.emails[0].value },
-              async (err, existingEmailUser) => {
-                if (err) {
-                  return done(err);
-                }
-                if (existingEmailUser) {
-                  console.log("errors", {
-                    msg: "There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.",
-                    existingEmailUser: existingEmailUser,
-                  });
-                  // done(err);
-                  done(null, existingEmailUser);
-                } else {
-                  const user = new User();
-                  user.name = profile.displayName;
-                  user.email = profile.emails[0].value;
-                  user.googleId = profile.id;
-                  user.tokens.push({ kind: "google", accessToken });
-                  user.profile.name = profile._json.name;
-                  user.profile.gender = profile._json.gender;
-                  user.profile.picture = profile._json.picture;
-
-                  // Create Stripe Customer
-                  const customer = await stripeService.addNewCustomer(
-                    user.profile.name,
-                    user.email
-                  );
-                  user.stripeCustomerId = customer.id;
-
-                  user.save((err) => {
-                    console.log("info", {
-                      msg: "Google account has been linked.",
-                      user: user,
-                      customer: customer,
-                    });
-                    done(err, user);
-                  });
-                }
-              }
+            // Create Stripe Customer
+            const customer = await stripeService.addNewCustomer(
+              user.profile.name,
+              user.email
             );
-          });
+            user.stripeCustomerId = customer.id;
+
+            user.save((err) => {
+              if (err) {
+                console.log("error", err);
+                return done(err);
+              }
+
+              console.log("info", {
+                msg: "Google account has been linked.",
+                user: user,
+                customer: customer,
+              });
+              done(null, user);
+            });
+          }
+        } else {
+          const existingUser = await User.findOne({ googleId: profile.id });
+
+          if (existingUser) {
+            done(null, existingUser);
+          } else {
+            const existingEmailUser = await User.findOne({
+              email: profile.emails[0].value,
+            });
+
+            if (existingEmailUser) {
+              console.log("errors", {
+                msg: "There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.",
+                existingEmailUser: existingEmailUser,
+              });
+              done(null, existingEmailUser);
+            } else {
+              const user = new User();
+              user.name = profile.displayName;
+              user.email = profile.emails[0].value;
+              user.googleId = profile.id;
+              user.tokens.push({ kind: "google", accessToken });
+              user.profile.name = profile._json.name;
+              user.profile.gender = profile._json.gender;
+              user.profile.picture = profile._json.picture;
+
+              // Create Stripe Customer
+              const customer = await stripeService.addNewCustomer(
+                user.profile.name,
+                user.email
+              );
+              user.stripeCustomerId = customer.id;
+
+              user.save((err) => {
+                if (err) {
+                  console.log("error", err);
+                  return done(err);
+                }
+
+                console.log("info", {
+                  msg: "Google account has been linked.",
+                  user: user,
+                  customer: customer,
+                });
+                done(null, user);
+              });
+            }
+          }
         }
       } catch (error) {
         console.log(error);
@@ -261,6 +162,145 @@ passport.use(
         done(error);
       } finally {
         passportGoogleStrategyAuthenticationTransaction.finish();
+      }
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "/auth/facebook/callback",
+      profileFields: ["id", "emails", "name"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const passportFacebookStrategyAuthenticationTransaction =
+        Sentry.startTransaction({
+          op: "passport.facebook.strategy.authentication",
+          description: "Facebook authentication",
+          name: "passport.facebook.strategy.authentication",
+        });
+
+      try {
+        if (profile._json.email) {
+          User.findOne(
+            { email: profile._json.email },
+            async (err, existingUser) => {
+              if (err) {
+                return done(err);
+              }
+              if (existingUser) {
+                return done(null, existingUser);
+              }
+              const user = new User();
+              user.email = profile._json.email;
+              user.name = `${profile.name.givenName} ${profile.name.familyName}`;
+              user.facebookId = profile.id;
+              user.tokens.push({ kind: "facebook", accessToken });
+              user.profile.name = user.name;
+              user.profile.picture = `https://graph.facebook.com/${profile.id}/picture?type=large`;
+
+              // Create Stripe Customer
+              const customer = await stripeService.addNewCustomer(
+                user.profile.name,
+                user.email
+              );
+              user.stripeCustomerId = customer.id;
+
+              user.save((err) => {
+                if (err) {
+                  return done(err);
+                }
+                console.log("info", {
+                  msg: "Facebook account has been linked.",
+                  user: user,
+                  customer: customer,
+                });
+                done(null, user);
+              });
+            }
+          );
+        } else {
+          console.log("errors", {
+            msg: "Facebook account doesn't have an email associated with it.",
+            profile: profile,
+          });
+          // done(null, false, { message: "Facebook account doesn't have an email associated with it." });
+          done(null, false);
+        }
+      } catch (error) {
+        console.log(error);
+        Sentry.captureException(error);
+        done(error);
+      } finally {
+        passportFacebookStrategyAuthenticationTransaction.finish();
+      }
+    }
+  )
+);
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "/auth/github/callback",
+      scope: "user:email",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const passportGitHubStrategyAuthenticationTransaction =
+        Sentry.startTransaction({
+          op: "passport.github.strategy.authentication",
+          description: "GitHub authentication",
+          name: "passport.github.strategy.authentication",
+        });
+
+      try {
+        if (profile.emails && profile.emails.length) {
+          User.findOne(
+            { email: profile.emails[0].value },
+            async (err, existingEmailUser) => {
+              if (err) {
+                return done(err);
+              }
+              if (existingEmailUser) {
+                return done(null, existingEmailUser);
+              }
+              const user = new User();
+              user.email = profile.emails[0].value;
+              user.githubId = profile.id;
+              user.tokens.push({ kind: "github", accessToken });
+              user.profile.name = profile.displayName;
+              user.profile.picture = profile._json.avatar_url;
+
+              // Create Stripe Customer
+              const customer = await stripeService.addNewCustomer(
+                user.profile.name,
+                user.email
+              );
+              user.stripeCustomerId = customer.id;
+
+              user.save((err) => {
+                console.log("info", {
+                  msg: "GitHub account has been linked.",
+                  user: user,
+                  customer: customer,
+                });
+                done(err, user);
+              });
+            }
+          );
+        } else {
+          return done("GitHub account email not verified.");
+        }
+      } catch (error) {
+        console.log(error);
+        Sentry.captureException(error);
+        done(error);
+      } finally {
+        passportGitHubStrategyAuthenticationTransaction.finish();
       }
     }
   )

a9e4171a7ffd6d3835a260e54a148cecf8884e5e
teetangh <teetangh@gmail.com>
2023-06-07 01:58:23 +0530

    improved the footer and pricing page

--- frontend/src/pages/info/pricing/PricingPage.jsx
+++ frontend/src/pages/info/pricing/PricingPage.jsx
@@ -1,16 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import "./PricingPage.scss";
 
 const PricingPage = () => {
+  const [isAnnual, setIsAnnual] = useState(true);
+
+  const toggleSubscription = () => {
+    setIsAnnual(!isAnnual);
+  };
+
   return (
-    <div>
-      {/* apply margin top */}
-      <div className="mt-100">
-        <h1>Pricing</h1>
+    <div className="content">
+      <div className="header">
+        <h1 className="text-center my-5">Pricing</h1>
+        <p className="motto">Affordable prices for everyone</p>
       </div>
-      Pricing Page for Bundles of packages
-      <div>1) Engineering Bundle </div>
-      <div>2) Arts Bundle </div>
-      <div>3) Commerce Bundle </div>
+      <div className="toggle-container" onClick={toggleSubscription}>
+        <p className="toggle-label">{isAnnual ? "Annual" : "Monthly"}</p>
+        <div className={`slider ${isAnnual ? "" : "monthly"}`}></div>
+      </div>
+      <div className="d-flex justify-content-center">
+        <div className="card pricing-card engineering-card">
+          <div className="card-header">Engineering Bundle</div>
+          <div className="card-body">
+            <ul>
+              <li>Feature 1: Lorem ipsum dolor sit amet.</li>
+              <li>Feature 2: Consectetur adipiscing elit.</li>
+              <li>Feature 3: Sed do eiusmod tempor incididunt.</li>
+              <li>Feature 4: Ut labore et dolore magna aliqua.</li>
+            </ul>
+            <div className="price-add-to-cart">
+              <p>{isAnnual ? "$100/year" : "$10/month"}</p>
+              <button className="btn btn-primary add-to-cart-btn">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card pricing-card arts-card">
+          <div className="card-header">Arts Bundle</div>
+          <div className="card-body">
+            <ul>
+              <li>Feature 1: Lorem ipsum dolor sit amet.</li>
+              <li>Feature 2: Consectetur adipiscing elit.</li>
+              <li>Feature 3: Sed do eiusmod tempor incididunt.</li>
+              <li>Feature 4: Ut labore et dolore magna aliqua.</li>
+            </ul>
+            <div className="price-add-to-cart">
+              <p>{isAnnual ? "$120/year" : "$12/month"}</p>
+              <button className="btn btn-primary add-to-cart-btn">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card pricing-card commerce-card">
+          <div className="card-header">Commerce Bundle</div>
+          <div className="card-body">
+            <ul>
+              <li>Feature 1: Lorem ipsum dolor sit amet.</li>
+              <li>Feature 2: Consectetur adipiscing elit.</li>
+              <li>Feature 3: Sed do eiusmod tempor incididunt.</li>
+              <li>Feature 4: Ut labore et dolore magna aliqua.</li>
+            </ul>
+            <div className="price-add-to-cart">
+              <p>{isAnnual ? "$150/year" : "$15/month"}</p>
+              <button className="btn btn-primary add-to-cart-btn">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/landing/landingcomponents/Footer.js
+++ frontend/src/pages/landing/landingcomponents/Footer.js
@@ -1,96 +1,149 @@
-import React from 'react';
-import styled from 'styled-components';
-import { InnerLayout } from '../styles/Layouts';
-import logo from '../img/logo.svg';
+import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../styles/Layouts";
+import logo from "../img/logo.svg";
+import appleStore from "../img3/apple-store.svg";
+import googlePlay from "../img3/google-play.svg";
 
 function Footer() {
-    return (
-        <FooterStyled>
-            <InnerLayout>
-                <div className="footer-con">
-                    <div className="logo-con">
-                       <div className="logo-items">
-                        <img src={logo} alt="" />
-                            <p>
-                                Copyright @2021 LoremIpsum. <br />
-                                All rights reserved.
-                            </p>
-                       </div>
-                    </div>
-                    <ul className="botton-nav">
-                        <div className="links1">
-                            <li>
-                                <a href="/team">Team</a>
-                            </li>
-                            <li>
-                                <a href="/press">Press</a>
-                            </li>
-                            <li>
-                                <a href="/fees">Fees</a>
-                            </li>
-                        </div>
-                        <div className="links2">
-                            <li>
-                                <a href="/services">Services</a>
-                            </li>
-                            <li>
-                                <a href="/projects">Projects</a>
-                            </li>
-                            <li>
-                                <a href="/affiliate">Affiliate</a>
-                            </li>
-                        </div>
-                        <div className="links3">
-                            <li>
-                                <a href="/terms">Terms of use</a>
-                            </li>
-                            <li>
-                                <a href="/privacy">Privacy Policy</a>
-                            </li>
-                            <li>
-                                <a href="/contact">Contact us</a>
-                            </li>
-                        </div>
-                    </ul>
-                </div>
-            </InnerLayout>
-        </FooterStyled>
-    )
+  return (
+    <FooterStyled>
+      <InnerLayout>
+        <div className="footer-con">
+          <div className="logo-con">
+            <img src={logo} alt="" />
+            <div className="logo-items">
+              <p>
+                Copyright @2021 LoremIpsum. <br />
+                All rights reserved.
+              </p>
+            </div>
+          </div>
+          <ul className="botton-nav">
+            <div className="links1">
+              <li>
+                <a href="/team">Team</a>
+              </li>
+              <li>
+                <a href="/press">Press</a>
+              </li>
+              <li>
+                <a href="/fees">Fees</a>
+              </li>
+            </div>
+            <div className="links2">
+              <li>
+                <a href="/services">Services</a>
+              </li>
+              <li>
+                <a href="/projects">Projects</a>
+              </li>
+              <li>
+                <a href="/affiliate">Affiliate</a>
+              </li>
+            </div>
+            <div className="links3">
+              <li>
+                <a href="/terms">Terms of use</a>
+              </li>
+              <li>
+                <a href="/privacy">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="/contact">Contact us</a>
+              </li>
+            </div>
+          </ul>
+
+          {/* Add office address */}
+          <address>
+            Kings & Queens Studio Rooms, Spice Garden, Marathahalli, Bengaluru,
+            Karnataka - 560037, India
+          </address>
+
+          {/* Add icons for downloading from Apple and Android stores */}
+          <div className="download-icons">
+            <img src={appleStore} alt="Download on the Apple Store" />
+            <img src={googlePlay} alt="Get it on Google Play" />
+          </div>
+        </div>
+      </InnerLayout>
+    </FooterStyled>
+  );
 }
 
 const FooterStyled = styled.footer`
-    padding: 0 18rem;
-    background-color: #DCE2F0;
-    @media screen and (max-width: 1347px){
-        padding: 5rem 14rem;
-    }
-    @media screen and (max-width: 1186px){
-        padding: 5rem 8rem;
-    }
-    @media screen and (max-width: 990px){
-        padding: 5rem 4rem;
-    }
-    .footer-con{
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
+  padding: 2rem 18rem;
+  background-color: #dce2f0;
+  background-image: linear-gradient(
+    to right,
+    #e0b0ff,
+    #ff69b4,
+    #9370db,
+    #4b0082
+  );
+  color: white;
+
+  @media screen and (max-width: 1347px) {
+    padding: 2rem 14rem;
+  }
+  @media screen and (max-width: 1186px) {
+    padding: 2rem 8rem;
+  }
+  @media screen and (max-width: 990px) {
+    padding: 2rem 4rem;
+  }
+
+  .footer-con {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 2rem;
+    align-items: center;
+    justify-content: center;
+
+    .logo-con {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 90px;
+        margin-right: 0.5rem;
+      }
     }
 
-    .logo-con{
-        display: flex;
-        align-items: center;
-        img{
-            width: 90px;
+    .botton-nav {
+      display: flex;
+      justify-content: space-between;
+
+      li {
+        padding: 0.5rem 0.5rem;
+        color: white;
+
+        a {
+          color: white;
+          font-size: 1.2rem;
         }
+      }
     }
 
-    .botton-nav{
-        display: flex;
-        justify-content: space-between;
-        li{
-            padding: 2rem 0;
-            color: #16194F;
-        }
+    address {
+      font-style: normal;
+      text-align: center;
+      margin-top: 1rem;
+      font-size: 1.2rem;
     }
+
+    .download-icons {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        width: 100px;
+        margin: 0.5rem;
+      }
+    }
+  }
 `;
 
-export default Footer
+export default Footer;
